d606c2a49593a9f7e5939e2edbff54b82f034fa3
realtnt <theo@thetaphoto.co.uk>
2022-02-12 14:58:40 +0000

    Added twitter and youtube functionality. Made it more responsive. Added some transitions to buttons.

--- linktree.js
+++ linktree.js
@@ -1,36 +1,77 @@
 
-const youtubeel = document.getElementById("youtube")
-let youtubeOpen = false
-const twitterel = document.getElementById("twitter")
+const youtubeEl = document.getElementById("youtube")
+const twitterEl = document.getElementById("twitter")
+const youtubecloseEl = document.getElementById("youtube-close")
+const twittercloseEl = document.getElementById("twitter-close")
+const twitterlinkEl = document.getElementById("twitter-link")
 let twitterOpen = false
+let youtubeOpen = false
 
-
-youtubeel.addEventListener("click", () => {
+youtubeEl.addEventListener("click", () => {
     if (youtubeOpen === false) {
-        youtubeel.innerHTML = `<span class="player">
-                                    Youtube OPEN
-                                </span>`
+        youtubecloseEl.setAttribute("class", "remove-style")
+        youtubeEl.setAttribute("class", "remove-press")
+        youtubeEl.innerHTML =
+            `<span class="player">
+                <iframe frameborder="0" allowfullscreen="1" 
+                title="YouTube video player" 
+                src="https://www.youtube.com/embed/9eDkxRL2yvc">
+                </iframe>
+            </span>
+        `
         youtubeOpen = true
-    } else {
-        youtubeel.innerHTML = `<span>
-                                    YouTube
-                                </span>`
-        youtubeOpen = false
     }
 })
 
+youtubecloseEl.addEventListener("click", () => {
+    youtubecloseEl.setAttribute("class", "hidden")
+    youtubeEl.innerHTML =
+        `<span>
+            YouTube
+        </span>
+    `
+    youtubeOpen = false
+})
 
-twitterel.addEventListener("click", () => {
+twitterEl.addEventListener("click", () => {
     if (twitterOpen === false) {
-        twitterel.innerHTML = `<span>
-                                    Twitter OPEN
-                                </span>`
+        twittercloseEl.setAttribute("class", "remove-style")
+        twitterEl.setAttribute("class", "remove-press")
+        twitterEl.innerHTML =
+            `<span id="twitter-link">
+                Twitter
+            </span>
+            <div id="tweet">
+            </div>
+            <a id="view-tweet-btn" href="https://twitter.com/scrimba/status/1492453432304685059" target="_blank">
+                View Tweet &nbsp;&nbsp;>
+            </a>
+        `
+        twttr.widgets.createTweet('1492453432304685059',
+            document.getElementById("tweet"),
+            {
+                theme: 'light',
+                conversation: 'none'
+            }
+        );
         twitterOpen = true
     } else {
-        twitterel.innerHTML = `<span>
-                                    Twitter
-                                </span>`
+        twittercloseEl.setAttribute("class", "hidden")
+        twitterEl.setAttribute("class", "")
+        twitterEl.innerHTML =
+            `<span id="twitter-link">
+                Twitter
+            </span>
+        `
         twitterOpen = false
     }
+})
 
+twittercloseEl.addEventListener("click", () => {
+    twittercloseEl.setAttribute("class", "hidden")
+    twitterEl.innerHTML =
+        `<span id="twitter-link">
+            Twitter
+        </span>`
+    twitterOpen = false
 })
